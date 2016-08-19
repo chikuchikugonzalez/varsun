07bb6f9c39e6c5f47844bae5982fab73027a9efe
chikuchikugonzalez <chikuchikugonzalez@gmail.com>
2016-08-19 23:34:16 +0900

    add tests

--- benches/lib.rs
+++ benches/lib.rs
@@ -17,7 +17,15 @@ fn mapfn(name: &str) -> Option<String> {
 }
 
 #[bench]
-fn posix_basic(b: &mut Bencher) {
+fn posix_nothing(b: &mut Bencher) {
+    let src = "foo bar baz";
+    b.iter(|| {
+        varsun::posix::substitute(src, mapfn);
+    });
+}
+
+#[bench]
+fn posix_simple(b: &mut Bencher) {
     let src = "${foo}";
     b.iter(|| {
         varsun::posix::substitute(src, mapfn);
@@ -25,8 +33,48 @@ fn posix_basic(b: &mut Bencher) {
 }
 
 #[bench]
-fn mswin_basic(b: &mut Bencher) {
+fn posix_once(b: &mut Bencher) {
+    let src = "foo, ${bar} and baz.";
+    b.iter(|| {
+        varsun::posix::substitute(src, mapfn);
+    });
+}
+
+#[bench]
+fn posix_twice(b: &mut Bencher) {
+    let src = "foo is ${foo}, bar is $bar.";
+    b.iter(|| {
+        varsun::posix::substitute(src, mapfn);
+    });
+}
+
+#[bench]
+fn mswin_nothing(b: &mut Bencher) {
+    let src = "foo bar baz";
+    b.iter(|| {
+        varsun::mswin::substitute(src, mapfn);
+    });
+}
+
+#[bench]
+fn mswin_simple(b: &mut Bencher) {
     let src = "%foo%";
+    b.iter(|| {
+        varsun::mswin::substitute(src, mapfn);
+    });
+}
+
+#[bench]
+fn mswin_once(b: &mut Bencher) {
+    let src = "foo, %bar% and baz.";
+    b.iter(|| {
+        varsun::mswin::substitute(src, mapfn);
+    });
+}
+
+#[bench]
+fn mswin_twice(b: &mut Bencher) {
+    let src = "foo is %foo%, baz is %baz%.";
     b.iter(|| {
         varsun::mswin::substitute(src, mapfn);
     });
